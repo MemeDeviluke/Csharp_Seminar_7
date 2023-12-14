ec634b4f5fb60525eb1afa837e6e4dcc11340232
Marsel Valeev <mars3l.valeev@yandex.ru>
2023-12-14 18:29:33 +0300

    Add solution of FirstTask

---
+++ FirstTask/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+int ReadInt(string text){
+
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
+
+void ShowNaturNumbers(int M, int N){
+
+    if(M > N){
+        return;
+    }
+    else{
+        System.Console.Write(M + " ");
+        ShowNaturNumbers(M + 1, N);
+    }
+
+}
+
+int M = ReadInt("Введите значение M (Значение M должно быть меньше N): ");
+int N = ReadInt("Введите значение N (Значение N должно быть больше M): ");
+
+ShowNaturNumbers(M, N);
